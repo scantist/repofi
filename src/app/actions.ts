3f4d45fe0cec0b863583e453d61aba15e95f9695
lizhongyue <lizhongyue248@163.com>
2025-02-27 22:11:20 +0800

    feat(PROD-7995):  Fix type

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,8 +1,9 @@
+"use server"
+
 import { type HomeSearchParams } from "~/lib/schema"
 import { daoRouter } from "~/server/api/routers/dao"
 import { daoService } from "~/server/service/dao"
 import { auth } from "~/server/auth"
-
 export const getDaoListAction = async (params: HomeSearchParams & { page?: number }) => {
   const session = await auth()
   const homeSearch = await daoService.homeSearch({ ...params },
