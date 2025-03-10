eb28fda2d2b3413022f04c30cce75c6d31be178d
klein <fanguorui@metatrust.io>
2025-03-10 21:47:05 +0800

    feat: add file upload

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,8 +1,55 @@
 "use server"
-
+import { Storage } from "@google-cloud/storage"
 import { type HomeSearchParams } from "~/lib/schema"
 import { daoService } from "~/server/service/dao"
 import { auth } from "~/server/auth"
+
+// Google Storage
+const GoogleStorageBucket = "repofi"
+const storage = new Storage()
+
+export const uploadFile = async ({
+                                   file,
+                                   fileName
+                                 }: {
+  file: string; // base64
+  fileName: string;
+}) => {
+  const buffer = Buffer.from(file, "base64")
+  const stream = storage
+    .bucket(GoogleStorageBucket)
+    .file(fileName)
+    .createWriteStream({
+      resumable: false
+    })
+
+  try {
+    const { url } = await new Promise<{
+      url: string;
+    }>((resolve, reject) => {
+      stream.on("finish", () => {
+        resolve({
+          url: `https://storage.googleapis.com/${GoogleStorageBucket}/${fileName}`
+        })
+      })
+      stream.on("error", (err) => {
+        reject(err)
+      })
+      stream.end(buffer)
+    })
+
+    return {
+      success: true,
+      url
+    }
+  } catch (err) {
+    console.log(`Fail to upload file: ${err as string}`)
+    return {
+      success: false,
+      message: "Fail to upload file"
+    }
+  }
+}
 export const getDaoListAction = async (params: HomeSearchParams & { page?: number }) => {
   const session = await auth()
   const homeSearch = await daoService.homeSearch({ ...params },
--- src/env.js
+++ src/env.js
@@ -21,6 +21,7 @@ export const env = createEnv({
     AUTH_GITLAB_SECRET: z.string(),
     AUTH_GITLAB_ID: z.string(),
     TOOL_REPO_GITHUB_ACCESS_TOKENS: z.string(),
+    GOOGLE_APPLICATION_CREDENTIALS: z.string(),
     CONTRACT_POC_ADDRESS: z.string().refine((v) => isAddress(v), "Invalid poc address"),
     CONTRACT_TOKENLOCKER_ADDRESS: z.string().refine((v) => isAddress(v), "Invalid tokenlocker address")
   },
@@ -47,6 +48,7 @@ export const env = createEnv({
     REDIS_PORT: process.env.REDIS_PORT,
     REDIS_DB: process.env.REDIS_DB,
     REDIS_PASSWORD: process.env.REDIS_PASSWORD,
+    GOOGLE_APPLICATION_CREDENTIALS: process.env.GOOGLE_APPLICATION_CREDENTIALS,
     NODE_ENV: process.env.NODE_ENV,
     NEXT_PUBLIC_CHAIN_ID: process.env.NEXT_PUBLIC_CHAIN_ID,
     NEXT_PUBLIC_REOWN_PROJECT_ID: process.env.NEXT_PUBLIC_REOWN_PROJECT_ID,
